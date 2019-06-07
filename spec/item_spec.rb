7c0b0c9653f3f144d88a512140d80f79b74112d2
Tara Sankhalpara <tsankhalpara@mac.com>
2019-06-07 14:56:34 +0100

    add test for Item class"

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,11 @@
+require 'item'
 
+describe Item do
+
+  describe '#to_s' do
+    it 'converts item to string' do
+      item = Item.new("foo", 0, 0)
+      expect(item.to_s()).to eq "foo, 0, 0"
+    end
+  end
+end
