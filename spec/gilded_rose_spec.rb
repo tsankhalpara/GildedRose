b821089a20b5a0669e3ba28e2ad588c484bd8412
Tara Sankhalpara <tsankhalpara@mac.com>
2019-06-07 12:04:23 +0100

    add test for quality reducing twice as fast

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -19,6 +19,34 @@ describe GildedRose do
           expect(subject.items[0].quality).to eq 19
         end
       end
+
+      context 'sell-in == 0 && quality > 0' do
+        before do
+         5.times { subject.update_quality }
+        end
+        it "reduces the sell-in by 1" do
+          subject.update_quality
+          expect(subject.items[0].sell_in).to eq -1
+        end
+        it "reduces the quality by 2" do
+          subject.update_quality
+          expect(subject.items[0].quality).to eq 13
+        end
+      end
+
+      context 'sell-in < 0 && quality == 0' do
+        before do
+         12.times { subject.update_quality }
+        end
+        it "reduces the sell-in by 1" do
+          subject.update_quality
+          expect(subject.items[0].sell_in).to eq -8
+        end
+        it "reduces the quality by 2" do
+          subject.update_quality
+          expect(subject.items[0].quality).to eq 0
+        end
+      end
     end
 
 
