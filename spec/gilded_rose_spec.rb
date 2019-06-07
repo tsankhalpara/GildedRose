89518bea864a79a4693c0d1a26c3c3b40d1747c1
Tara Sankhalpara <tsankhalpara@mac.com>
2019-06-07 12:12:04 +0100

    add tests for aged brie

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -42,9 +42,50 @@ describe GildedRose do
           subject.update_quality
           expect(subject.items[0].sell_in).to eq -8
         end
-        it "reduces the quality by 2" do
+        it "quality does not fall below 0" do
           subject.update_quality
           expect(subject.items[0].quality).to eq 0
+        end
+      end
+    end
+
+    describe "Aged Brie" do
+      context 'sell-in > 0 && quality == 0' do
+        it "reduces the sell-in by 1" do
+          subject.update_quality
+          expect(subject.items[1].sell_in).to eq 1
+        end
+        it "increases the quality by 1" do
+          subject.update_quality
+          expect(subject.items[1].quality).to eq 1
+        end
+      end
+
+      context 'sell-in == 0 && quality > 0' do
+        before do
+         2.times { subject.update_quality }
+        end
+        it "reduces the sell-in by 1" do
+          subject.update_quality
+          expect(subject.items[1].sell_in).to eq -1
+        end
+        it "increases the quality by 2" do
+          subject.update_quality
+          expect(subject.items[1].quality).to eq 4
+        end
+      end
+
+      context 'sell-in < 0 && quality == 50' do
+        before do
+         26.times { subject.update_quality }
+        end
+        it "reduces the sell-in by 1" do
+          subject.update_quality
+          expect(subject.items[1].sell_in).to eq -25
+        end
+        it "quality does not go above 50" do
+          subject.update_quality
+          expect(subject.items[1].quality).to eq 50
         end
       end
     end
